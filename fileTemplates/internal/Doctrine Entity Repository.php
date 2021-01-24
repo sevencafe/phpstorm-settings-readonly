4735191b5b91c621571a58521b4b55a0efe83917
akaimononoke <p.m.byzance0817@gmail.com>
2021-01-25 00:50:11 +0900

    2021-01-25 00:50:11

---
+++ fileTemplates/internal/Doctrine Entity Repository.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace ${NAMESPACE};
+
+use ${ENTITY_NAMESPACE}\\${ENTITY_NAME};
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * @method ${ENTITY_NAME}|null find(\$id, \$lockMode = null, \$lockVersion = null)
+ * @method ${ENTITY_NAME}|null findOneBy(array \$criteria, array \$orderBy = null)
+ * @method ${ENTITY_NAME}[] findAll()
+ * @method ${ENTITY_NAME}[] findBy(array \$criteria, array \$orderBy = null, \$limit = null, \$offset = null)
+ */
+class ${NAME} extends EntityRepository
+{
+}
